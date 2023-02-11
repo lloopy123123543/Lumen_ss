f1cd53f5731836e574ef23a5d2c6c739ccf17dbf
Парфёнов Игорь Николаевич <pl8422@PL1.EDU>
2023-02-11 14:48:32 +0500

    ssss

---
+++ database/migrations/2023_02_11_081011_create_announcement_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnnouncementTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('announcement', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title_ad');
+            $table->string('info_ad');
+            $table->string('contact_name');
+            $table->string('contact_phone');
+            $table->string('date_end');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('announcement');
+    }
+}
